cd030dc68350d9013c14291fa63977433f3c3be1
lucas-eiki <lucas.gushiken@sptech.school>
2025-10-16 18:46:16 -0300

    feita a página de perfil

--- Projeto/Site-Gibbon/js/dashboard.js
+++ Projeto/Site-Gibbon/js/dashboard.js
@@ -1,3 +1,9 @@
 function sair() {
     window.location.href = '../index.html'
 }
+
+function atualizarPerfil() {
+    document.getElementById('senha-atual').value = ''
+    document.getElementById('nova-senha').value = ''
+    document.getElementById('confirmacao-senha').value = ''
+}
