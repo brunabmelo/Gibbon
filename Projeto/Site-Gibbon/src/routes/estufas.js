264fea8682ffd80d9c3bb1dc3551f54f95145df6
lucas-eiki <lucas.gushiken@sptech.school>
2025-11-27 21:42:39 -0300

    melhoria(estufas): corrige erro na rota

--- Projeto/Site-Gibbon/src/routes/estufas.js
+++ Projeto/Site-Gibbon/src/routes/estufas.js
@@ -7,7 +7,7 @@ router.get("/listar/:id_empresa", function (req, res) {
     estufasController.listar(req, res);
 });
 
-router.get('/cadastrar:id_empresa', function (req, res) {
+router.get('/cadastrar/:id_empresa', function (req, res) {
     estufasController.cadastrar(req, res)
 })
 
