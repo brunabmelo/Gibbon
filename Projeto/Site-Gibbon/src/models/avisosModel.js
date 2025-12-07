cae4d8d0e424ec4bd2ead332f6d6b0e195297e30
lucas-eiki <lucas.gushiken@sptech.school>
2025-12-07 18:44:56 -0300

    melhoria(avisos): corrige renderização de sensores fora do ideal

--- Projeto/Site-Gibbon/src/models/avisosModel.js
+++ Projeto/Site-Gibbon/src/models/avisosModel.js
@@ -3,7 +3,8 @@ var database = require("../database/config");
 function buscar(ID_EMPRESA, dataHora) {
     var instrucaoSql = `
     SELECT sensor, estufa FROM vw_avisos
-    WHERE empresa = ${ID_EMPRESA} AND dataHora < '${dataHora}' AND foraIdeal = 1;
+    WHERE empresa = ${ID_EMPRESA} AND dataHora > '${dataHora}' AND foraIdeal = 1
+    GROUP BY sensor, estufa;
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
