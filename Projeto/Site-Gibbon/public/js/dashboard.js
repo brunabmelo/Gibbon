0974d712fa289bee5f61e05f4a2e0ff288627052
lucas-eiki <lucas.gushiken@sptech.school>
2025-12-07 20:42:15 -0300

    remoção(dashboard.js): remove funções desnecessárias

--- Projeto/Site-Gibbon/public/js/dashboard.js
+++ Projeto/Site-Gibbon/public/js/dashboard.js
@@ -1,35 +1,6 @@
-let senhaUsuario = '123456'
-
 function sair() {
     sessionStorage.clear();
     window.location.href = '../index.html'
-}
-
-function atualizarPerfil() {
-    let iptSenhaAtual = document.getElementById('senha-atual')
-    let iptNovaSenha = document.getElementById('nova-senha')
-    let iptConfirmacaoSenha = document.getElementById('confirmacao-senha')
-
-    if (
-        iptSenhaAtual.value == '' ||
-        iptNovaSenha.value == '' ||
-        iptConfirmacaoSenha.value == ''
-    ) {
-        alert('Preencha todos os campos')
-    }
-    else if (iptSenhaAtual.value != senhaUsuario) {
-        alert('Senha incorreta')
-    }
-    else if (iptNovaSenha.value != iptConfirmacaoSenha.value) {
-        alert('Senhas não correspondem')
-    }
-    else {
-        iptSenhaAtual.value = ''
-        iptNovaSenha.value = ''
-        iptConfirmacaoSenha.value = ''
-
-        alert('Senha atualizada com sucesso')
-    }
 }
 
 function validarSessao() {
